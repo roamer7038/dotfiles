7ab28c5bc28cbd685a2e4d62da51096e73a3147d
roamer7038 <okayado7038@gmail.com>
2019-08-23 18:44:19 +0900

    リファクタリング

--- setup.sh
+++ setup.sh
@@ -1,53 +1,48 @@
 #!/bin/sh
 
-# run this script directory path
+# Where this script is running
 PWD=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
 
-ln -sf $PWD/.bash_profile ~/.bash_profile
-ln -sf $PWD/.bashrc ~/.bashrc
-ln -sf $PWD/.tmux.conf ~/.tmux.conf
-ln -sf $PWD/.vimrc ~/.vimrc
-ln -sf $PWD/.zshrc ~/.zshrc
-ln -sf $PWD/.gitconfig ~/.gitconfig
+# Basic dotfiles
+DOTFILES=(.bashrc .zshrc .tmux.conf .vimrc .gitconfig .latexmkrc)
+for file in ${DOTFILES[@]}; do
+  ln -s $PWD/$file $HOME/$file
+done
 
-## nox on Linux Server 
-getopts "s" opts
-if [ $opts = "s" ]; then
+# Vim's config
+# With option the "-nox", the ".vim.nox" is duplicated.
+# By default, the ".vim" directory is created and the files are duplicated.
+getopts "nox" opts
+if [ $opts = "nox" ]; then
   ln -sf $PWD/.vimrc.nox ~/.vimrc
   exit 0
+else
+  mkdir -p $HOME/.vim
+
+  VIMCONF=($(ls $PWD/.vim))
+  for file in ${VIMCONF[@]}; do
+    ln -sf $PWD/.vim/$file $HOME/.vim/$file
+  done
 fi
 
 case ${OSTYPE} in
   darwin*)
-    xcode-select --install
-    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-    echo 'Please enter GitHub Username...'
-    read GITHUB_USERNAME 
-    TOKEN=$(curl -u $GITHUB_USERNAME -d '{"scopes":["repo"],"note":"'$(date +"%Y%m%d%I%M%S")'"}' https://api.github.com/authorizations | grep -w 'token' | awk -F '"' '{ print $4 }')
-    echo 'export HOMEBREW_GITHUB_API_TOKEN="'$TOKEN'"' > ~/.github_api_token
-    exec $SHELL -l
-    brew tap homebrew/bundle
-    cd $PWD
-    brew bundle
     ;;
   linux*)
-    ## X Window System
-    ln -sf $PWD/.Xmodmap ~/.Xmodmap
-    ln -sf $PWD/.xprofile ~/.xprofile
-    ln -sf $PWD/.compton.conf ~/.compton.conf
-    ## i3 Window Manager
-    mkdir -p ~/.config/i3
-    ln -sf $PWD/.config/i3/config ~/.config/i3/config
-    mkdir -p ~/.config/i3status
-    ln -sf $PWD/.config/i3status/config ~/.config/i3status/config
-    ## Terminal Emulator
-    mkdir -p ~/.config/terminator
-    ln -sf $PWD/.config/terminator/config ~/.config/terminator/config
-    mkdir -p ~/.config/alacritty
-    ln -sf $PWD/.config/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
-    ## Other Software
-    mkdir -p ~/.config/dunst
-    ln -sf $PWD/.config/dunst/dunstrc ~/.config/dunst/dunstrc
-    ln -sf $PWD/.latexmkrc ~/.latexmkrc
-    ;;
+    # X Window System's config
+    XCONFIG=(.Xmodmap .xprofile .compton.conf)
+    for file in ${XCONFIG[@]}; do
+      ln -s $PWD/$file $HOME/$file
+    done
+
+    # Application's config
+    mkdir -p $HOME/.config
+    APPCONFDIR=(i3 i3status terminator alacritty dunst)
+    for dir in ${APPCONFDIR[@]}; do
+      mkdir -p $HOME/.config/$dir
+      APPCONF=($(ls $PWD/.config/$dir))
+      for file in ${APPCONF[@]}; do
+        ln -sf $PWD/.config/$dir/$file $HOME/.config/$dir/$file
+      done
+    done
 esac
